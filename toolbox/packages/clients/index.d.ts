9a8f1242586e8a1cd704fa31fae2c0c93f93fea7
Kashyap Maheshwari <kashyapmaheshwari@Kashyaps-iMac.cinci.rr.com>
2025-08-22 12:31:46 -0400

    chore: migrate toolbox docs to ops; archive src snippets; remove repo-level toolbox

--- toolbox/packages/clients/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * as appwrite from "./src/appwrite";
-export * as openai from "./src/openai-responses";
-export * as anthropic from "./src/anthropic-messages";
-export * as meshy from "./src/meshy";
-export * as runway from "./src/runway";
--- toolbox/packages/clients/index.ts
+++
@@ -1,7 +0,0 @@
-// <toolbox:BEGIN toolbox/packages/clients/index.ts v1>
-export * as appwrite from "./src/appwrite";
-export * as openai from "./src/openai-responses";
-export * as anthropic from "./src/anthropic-messages";
-export * as meshy from "./src/meshy";
-export * as runway from "./src/runway";
-// <toolbox:END toolbox/packages/clients/index.ts v1>
